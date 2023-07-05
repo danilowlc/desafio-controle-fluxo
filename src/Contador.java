072e08ba4ee2975fe5807748dbea17285e30ff91
danilowlc <danilowlc@gmail.com>
2023-07-04 21:34:18 -0300

    fechando o Scanner de forma explicita

--- src/Contador.java
+++ src/Contador.java
@@ -11,6 +11,7 @@ public class Contador {
         try {
             // chamando o método contendo a lógica de contagem
             contar(parametroUm, parametroDois);
+            terminal.close();
 
         } catch (ParametrosInvalidosException exception) {
             // imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
